302e5d4f9628414932d9d47f7be1e48ff42b1de9
Sergey Chizhik <s.s.chizhik@gmail.com>
2016-10-16 14:02:14 +0300

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -v /tmp/ingress-intel-total-conversion:/iitc -i -t ahazem/android:v0.7.6 /bin/bash 
+docker run -v `pwd`:/iitc -i -t --rm -w /iitc ahazem/android:v0.7.6 /bin/bash
